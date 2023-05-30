76d268f9c46c5c41d00a1c869e54bf71fa249402
pvtcompyle <43185139+pvtcompyle@users.noreply.github.com>
2023-05-30 00:42:12 -0500

    Delete add_path.sh
    this was a poor way of handling this.

--- add_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo # add LinFu to path >> ~/.bashrc
-echo export PATH="$HOME/Documents/GitHub/LinFu/cmd:$PATH" >> ~/.bashrc
-echo # add Linfu to path >> ~/.zshrc
-echo export PATH="$HOME/Documents/GitHub/LinFu/cmd:$PATH" >> ~/.zshrc
-
-export PATH="$HOME/Documents/GitHub/LinFu/cmd:$PATH"
-sudo chmod +x $HOME/Documents/GitHub/LinFu/cmd/*
